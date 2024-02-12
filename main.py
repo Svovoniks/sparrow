616f85f61ad30514d3463d32889a288b96619eea
Svovoniks <svovoniks@gmail.com>
2024-02-12 12:25:55 +0300

    added ui

--- main.py
+++ main.py
@@ -5,32 +5,70 @@ import asyncio
 import subprocess
 import json
 from os import listdir
-from os.path import exists
+from os.path import exists, isdir
 
 from magnet2torrent import Magnet2Torrent, FailedToFetchException
 
 CONFIG_FILE = "sys_torrent.cfg"
 REQUIRED_CONFIG_FIELDS = ['download_dir', 'title_list']
 
+class UI:
+    
+    def __init__(self, config) -> None:
+        self.config = config
+    
+    @staticmethod
+    def start():
+        ui = UI.setup_UI()
+        
+        
+    
+    @staticmethod
+    def ask_for_config():
+        print("I couldn't find a config file")
+        print("So let's get you started")        
+        print("Where would you like me to put new episodes?")
+        
+        download_dir = str(input())
+        
+        while not isdir(download_dir):
+            print("Error: not a real folder")
+            print("Try again")
+            
+            download_dir = str(input())
+        
+        Configuration.create_config(download_dir, [])
+        
+        print("You are all set")
+        print("Please don't remove last episode of each show")
+        print("so i know what you've watched already")
+    
+    
+    @staticmethod
+    def setup_UI(self):
+        config = Configuration.try_parse_config()
+        
+        if config == None:
+            config = UI.ask_for_config()
+            
+        return UI(config)
+
 class Configuration:
     
-    def __init__(self, download_dir, title_list, config_location) -> None:
+    def __init__(self, download_dir, title_list) -> None:
         self.download_dir = download_dir
         self.title_list = title_list
-        self.config_location = config_location
-        
-        
-        
-    # assumes CONFIG_FILE is a valid configuration
-    # returns a Configuration object
+    
+    
+    # returns a Configuration object if CONFIG_FILE is valid
     @staticmethod
-    def parse_config():
-        config_json = None
+    def try_parse_config():
+        config_json = Configuration.get_config_jscn()
         
-        with open(CONFIG_FILE) as file:
-            config_json = json.load(file)
-        
-        return Configuration(config_json['download_dir'], config_json['title_list'], CONFIG_FILE)
+        if config_json == None:
+            return None
+                
+        return Configuration(config_json['download_dir'], config_json['title_list'])
     
     
     # adds title to the title list BUT doesn't save it to the disk
@@ -52,35 +90,39 @@ class Configuration:
             json.dump(self.build_json(), file)
     
     
-    # makes config_json a valid configuration
+    # creates full config in CONFIG_FILE file and returns Configuration 
     @staticmethod
-    def setup_config(config_json):
-        pass
+    def create_config(download_dir, title_list):
+        config = Configuration(download_dir,  title_list)
+        
+        with open(CONFIG_FILE) as file:
+            json.dump(config.build_json())
+            
+        return config
     
     
     # checks if all required fields are present in config_json
     @staticmethod
-    def check_config(config_json):
+    def check_config_json(config_json):
         return all([field in config_json for field in REQUIRED_CONFIG_FIELDS])
     
     
-    # checks CONFIG_FILE and puts a valid config by that path if it's not there already
-    # reuturns valid config_json
+    # if CONFIG_FILE is a valid config file returns valid config_json
     @staticmethod
-    def get_or_create_config_json():
+    def try_get_config_jscn():
         if not exists(CONFIG_FILE):
-            with open(CONFIG_FILE, "w"): 
-                pass
-        
-        config_json = None
+            return None
         
         with open(CONFIG_FILE) as file:
-            config_json = json.load(file)
-            
-        if not Configuration.check_config(config_json):
-            Configuration.setup_config(config_json)
-        
-        return config_json
+            try:
+                config_json = json.load(file)
+                
+                if Configuration.check_config_json(json.load(file)):
+                    return config_json
+                
+                return None
+            except:
+                return None
 
 
 
@@ -117,11 +159,15 @@ class MagnetChecker:
             print('Couldnt check magnet link')
         
         return None
-    
+
+
+
 class TitleCheckResult:
     def __init__(self) -> None:
         self.pages = []
         self.parser = None
+
+
 
 class ParserInterface:
     def __init__(self, download_folder_contents) -> None:
@@ -139,7 +185,8 @@ class ParserInterface:
             return None
         
         return resp
-        
+
+
 
 class SubsPleaseParser(ParserInterface):
     def __init__(self, download_folder_contents) -> None:
@@ -221,7 +268,7 @@ class SubsPleaseParser(ParserInterface):
             to_download.append(i)
             
         return to_download
-        
+
 
 
 # if __name__ == "__main__":
