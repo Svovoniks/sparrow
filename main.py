664c871d5be537b2e653049bd53f093c36d2f814
Svovoniks <svovoniks@gmail.com>
2024-03-10 18:49:38 +0300

    testing in progress

--- main.py
+++ main.py
@@ -1,6 +1,24 @@
 from src.UI import UI
 from colorama import just_fix_windows_console
 
-if __name__ == "__main__":
-    just_fix_windows_console()
-    UI.start()
+# if __name__ == "__main__":
+#     just_fix_windows_console()
+#     UI.start()
+    
+from src.TorrentUtils import MagnetChecker
+import asyncio
+
+mg = MagnetChecker(r'''magnet:?xt=urn:btih:8A25DC6474D11A41A0B68EFAF26D9243555DF6B5&dn=Scavengers%20Reign%20S01%202023%201080p%20WEB%20RIP%20AV1%20Opus%20NeoNyx343&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.t-rb.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker1.bt.moack.co.kr%3A80%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.mirrorbay.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.theoks.net%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.moeking.me%3A6969%2Fannounce&tr=udp%3A%2F%2Fmovies.zsw.ca%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.therarbg.to%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.therarbg.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce''')
+
+from magnet2torrent import Magnet2Torrent, FailedToFetchException
+
+async def fetch_that_torrent():
+    m2t = Magnet2Torrent("magnet:?xt=urn:btih:8A25DC6474D11A41A0B68EFAF26D9243555DF6B5&dn=Scavengers%20Reign%20S01%202023%201080p%20WEB%20RIP%20AV1%20Opus%20NeoNyx343&tr=udp%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2Fopen.demonii.com%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.t-rb.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker1.bt.moack.co.kr%3A80%2Fannounce&tr=udp%3A%2F%2Fexplodie.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce&tr=udp%3A%2F%2Ftracker.mirrorbay.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.theoks.net%3A6969%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Fexodus.desync.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.moeking.me%3A6969%2Fannounce&tr=udp%3A%2F%2Fmovies.zsw.ca%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.therarbg.to%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.therarbg.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce")
+    try:
+        filename, torrent_data = await m2t.retrieve_torrent()
+        print(filename)
+        print(torrent_data)
+    except FailedToFetchException:
+        print("Failed")
+
+print(asyncio.run(fetch_that_torrent()))
