0335252cb045aa9f6384ca06e311b57c5f9d5a58
Svovoniks <svovoniks@gmail.com>
2025-03-02 01:14:25 +0300

    fix requirements

--- create_launcher.bat
+++ create_launcher.bat
@@ -4,6 +4,7 @@ echo py main.py>> start.bat
 
 echo cd %cd%> update.bat
 echo git pull https://github.com/Svovoniks/sparrow.git>> update.bat
+echo pip install -r requirements.txt>> update.bat
 
 echo cd %cd%> update_all.bat
 echo call venv\Scripts\activate>> update_all.bat
